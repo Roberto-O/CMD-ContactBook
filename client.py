a35dc92e890b337fb18931082ec9274401117707
ranallor <ranallra2020@mountunion.edu>
2019-12-09 11:03:10 -0500

    Search by full

--- client.py
+++ client.py
@@ -17,15 +17,21 @@ def createTable():
     conn.commit()
 
 def searchByLast(input):
-    search = "SELECT last_name, first_name FROM contacts WHERE last_name LIKE" + input + "%'"
+    search = "SELECT last_name, first_name FROM contacts WHERE last_name LIKE '" + input + "%'"
+    print(search)
     c.execute(search)
     conn.commit()
 
 def searchByFirst(input):
-    search = "SELECT last_name, first_name FROM contacts WHERE first_name LIKE" + input + "%'"
+    search = "SELECT last_name, first_name FROM contacts WHERE first_name LIKE '" + input + "%'"
     c.execute(search)
     conn.commit()
 
+def searchByFull(input_l, input_f):
+    WHERE CONCAT(customers.first_name, ' ', customers.last_name) LIKE '%John Smith%'
+    search = "SELECT last_name, first_name FROM contacts WHERE CONCAT(first_name, ' ', last_name) LIKE '%" + input_f + " " + input_l "%'"
+    c.execute(search)
+    conn.commit()
 
 
 
--- contacts.py
+++ contacts.py
@@ -30,7 +30,7 @@ def showContacts():
 @click.command()
 def searchContacts():
     click.echo('--- Search Contacts - Select Number Option ---')
-    option = click.prompt('1 - Search by last name\n2 - Search by first name\n3 - Search by full name\n4 - Search by company\n5 - Search by address\n6 - Search by phone number\n7 - Return to main menu')
+    option = click.prompt('1 - Search by last name\n2 - Search by first name\n3 - Search by full name\n4 - Search by company\n5 - Search by address\n6 - Search by phone number\n7 - Return to main menu\n')
     if(option == '1'):
         input = click.prompt('Enter Last Name')
         client.searchByLast(input)
